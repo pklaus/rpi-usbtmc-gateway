b8013f90bd5d4a2a1a930b1092b31639ae6b1676
Philipp Klaus <philipp.klaus@gmail.com>
2015-08-03 17:25:07 +0200

    Using the same port (5025) in all the server scripts

--- rpi/servers/nc.sh
+++ rpi/servers/nc.sh
@@ -2,6 +2,6 @@
 
 : ${DIR:?"Need to set the DIR environment variable"}
 
-echo "Starting USBTMC server on port 8000" 
+echo "Starting USBTMC server on port 5025" 
 insmod "$DIR/kernel-module/usbtmc.ko"
-while true; do nc -l -p 8000 -c "$DIR/userland/talk2scope.sh" ; done
+while true; do nc -l -p 5025 -c "$DIR/userland/talk2scope.sh" ; done
